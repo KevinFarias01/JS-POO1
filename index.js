5a5c9fba1485e556edcc2455ae9feea09a6f9239
Kevin Farias <kevin.farias.rib@gmail.com>
2021-11-25 11:50:21 -0300

    1

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
-console.log(contaPoupanca);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+
 
 console.log(contaCorrenteRicardo);
